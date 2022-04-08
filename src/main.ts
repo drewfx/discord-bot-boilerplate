01e3c84ed0b631088d71d5cff5bdcd7e9081c42d
Drew Ruppel <drewruppel@gmail.com>
2022-04-08 09:22:46 -0700

    temp save

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import Config from './Core/Config'
 import {Client, Intents, Interaction} from 'discord.js'
+import {SlashCommandBuilder} from '@discordjs/builders'
+import {REST} from '@discordjs/rest'
+import {Routes} from 'discord-api-types/v10'
+
+
+// We need to break this up into two portions, a register.ts that fires off to register commands.
+// Then the actual bot logic
+new Routes()
 
 // Retrieve .env
 const config = new Config().get()
@@ -10,7 +18,11 @@ const client = new Client(
 )
 
 // @todo: Get Commands
-const commands: Command[] = []
+const commands: SlashCommandBuilder[] = [
+  new SlashCommandBuilder()
+]
+
+const rest = new REST({ version: '9' }).setToken(config.TOKEN);
 
 client.on('ready', () => {
   if (!client.user || !client.application) {
@@ -24,7 +36,11 @@ client.on('ready', () => {
     '|___| \\_/ \\___|      |___|\\__||_||_|\\___/\\___|/__/\n'
   )
 
-  await client.application.commands.set(commands);
+  void client.application.commands.set(commands);
+})
+
+client.on('message', msg => {
+  console.log(`[message log] ${msg.content}`)
 })
 
 client.on('interactionCreate', async (interaction: Interaction) => {
